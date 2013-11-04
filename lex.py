9d51f7dfa2d49d098933600d06eb5f08a6192201
Ingrid Avendaño <ingridavendano@gmail.com>
2013-11-04 14:35:54 -0800

    Reads VHLD code into Module class.

--- lex.py
+++ lex.py
@@ -2,28 +2,44 @@
 
 # Lexer file that reads in VHDL code and parses it. 
 
+from netlist import Module
+
 FILENAME = "vhdl/and_gate.vhd"
-
-
-
-def tokenize(code):
-	pass
-
-
-def find_ports(code):
-	pass
+COMMENTED_CODE = "--"
 
 ## READIN AND FILTERING CODE ####################################
-
-# finds open and closed brackets of code:
-def bracketed_code(code):
-	pass
-
 
 def separate_sections(code):
 	definition = [] 
 	behavior = []
-	pass
+	
+	# figuring out the definition of a circuit by finding the beginning
+	# and ending points of a circuit
+	def_begin = def_end = False
+	architecture_section = False
+
+	for line in code:
+		if "entity" in line:
+			def_begin = True
+
+		# adds line of definition code
+		if def_begin and not def_end:
+			definition.append(line)
+
+		if "end" in line:
+			def_end = True
+
+		if "architecture" in line:
+			architecture_section = True
+
+		if architecture_section: 
+			behavior.append(line)
+
+	def_class = Module(definition)
+	def_class.show()
+
+	return definition, behavior
+
 
 
 def filter_code(code):
@@ -31,18 +47,21 @@ def filter_code(code):
 
 	# removes blank lines of code and comments
 	for line in code:
-		if line != "" and not "--" in line:
+		if line != "" and not COMMENTED_CODE in line:
 			# remove use of libraries
 			if not "library" in line and not "use" in line:
 				cleaner_code.append(line)
 
-	print_code(cleaner_code)
 	return cleaner_code
 
 
 def print_code(code):
+	print ""
+	print "#"*80
 	for line in code:
 		print line
+	print "#"*80
+	print ""
 
 
 def read(file_name):
@@ -58,10 +77,8 @@ def read(file_name):
 ## MAIN #########################################################
 def main():
 	vhdl = read(FILENAME)
-	# print_code(vhdl)
 	vhdl = filter_code(vhdl)
 	definition, behavior = separate_sections(vhdl)
-
 
 
 if __name__ == "__main__":
