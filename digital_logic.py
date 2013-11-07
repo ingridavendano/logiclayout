fb175076646472871449f6f86b0b147217e84341
Ingrid Avendaño <ingridavendano@gmail.com>
2013-11-06 23:20:37 -0800

    Parse through digitial logic here.

---
+++ digital_logic.py
@@ -0,0 +1,19 @@
+# Created by Ingrid Avendano 11/6/13.
+
+# The lexer file that reads in VHDL. 
+
+
+
+# list of tokens
+tokens = [
+	''
+
+]
+
+
+def main():
+	pass
+
+
+if __name__ == "__main__":
+	main()
