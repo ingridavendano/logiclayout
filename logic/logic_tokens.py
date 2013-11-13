37996d928d4f31368d16fa4253699de44c7aae71
Ingrid Avendaño <ingridavendano@gmail.com>
2013-11-12 21:45:01 -0800

    Defining logic tokens to make parse trees.

---
+++ logic/logic_tokens.py
@@ -0,0 +1,13 @@
+# -----------------------------------------------------------------------------
+# logic_tokens.py
+# Created by Ingrid Avendano 11/12/13.
+#
+# Test that boolean algebra tokens are tokenized properly.
+# -----------------------------------------------------------------------------
+
+
+# token class template
+class TokenTemplate:
+	def __init__(self, value=0):
+		self.value = value
+
