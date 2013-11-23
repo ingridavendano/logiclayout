430778ba100374dc7342a394c9b22c115962fa75
Ingrid Avendaño <ingridavendano@gmail.com>
2013-11-22 19:08:41 -0800

    Running tests not through web interface.

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ def compile_expr(expr):
 if __name__ == "__main__":
 
 	expr = "f = a * (b OR (NOT (c * (NOT i)))) xor (d | c | g) xor (s OR t) xor (NOT (a OR (NOT (u and p)))) and f"
+	# expr = "f = (a + b) and c"
 	tree = run.compiler(expr)
 
 	# tree.print_tree()
