5e321701e94d175d7f9f940a87af94bc03b7345b
Ingrid Avendaño <ingridavendano@gmail.com>
2013-12-17 19:26:35 -0800

    Tokens defined using regrex.

--- compiler/lexer.py
+++ compiler/lexer.py
@@ -6,21 +6,21 @@
 # -----------------------------------------------------------------------------
 
 tokens = [
-	# identifier
-	'ID',
+    # identifier
+    'ID',
 
-	# logic operatiors 
-	'NOT', 'NAND', 'AND', 'NOR', 'OR', 'NXOR', 'XOR',
-	
-	# assigments
-	'EQUALS',
+    # logic operatiors 
+    'NOT', 'NAND', 'AND', 'NOR', 'OR', 'NXOR', 'XOR',
+    
+    # assigments
+    'EQUALS',
 
-	# delimiters 
-	'LPAREN', 'RPAREN', 'LBRACKET', 'RBRACKET', 'LBRACE', 'RBRACE',
+    # delimiters 
+    'LPAREN', 'RPAREN', 'LBRACKET', 'RBRACKET', 'LBRACE', 'RBRACE',
 
-	# binary numbers, integers, booleans
-	'BIN', 'INT', 'TRUE', 'FALSE'
-	]
+    # binary numbers, integers, booleans
+    'BIN', 'INT', 'TRUE', 'FALSE'
+    ]
 
 # -----------------------------------------------------------------------------
 
@@ -29,32 +29,32 @@ t_ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
 # logic operators 
 def t_NOT(t):
-	r'([nN][oO][tT])|~|!'
-	return t
+    r'([nN][oO][tT])|~|!'
+    return t
 
 def t_NAND(t):
-	r'([nN][aA][nN][dD])'
-	return t
+    r'([nN][aA][nN][dD])'
+    return t
 
 def t_AND(t):
-	r'([aA][nN][dD])|(\&{1,2})|\*'
-	return t
+    r'([aA][nN][dD])|(\&{1,2})|\*'
+    return t
 
 def t_NOR(t):
-	r'([nN][oO][rR])'
-	return t
+    r'([nN][oO][rR])'
+    return t
 
 def t_OR(t):
-	r'([oO][rR])|\|{1,2}|\+'
-	return t
+    r'([oO][rR])|\|{1,2}|\+'
+    return t
 
 def t_NXOR(t):
-	r'([nN][xX][oO][rR])'
-	return t
+    r'([nN][xX][oO][rR])'
+    return t
 
 def t_XOR(t):
-	r'([xX][oO][rR])|\^'
-	return t
+    r'([xX][oO][rR])|\^'
+    return t
 
 # assignments
 t_EQUALS = r'='
@@ -67,49 +67,49 @@ t_RBRACKET = r'\]'
 t_LBRACE = r'\{'
 t_RBRACE = r'\}'
 
-# tokens to ignore
-t_ignore = " \t"
-
 # binary
 def t_BIN(t):
-	r'(0b)[01^(2-9)]+'
-	try:
-		t.value = int(t.value,2)
-	except ValueError:
-		print "Binary value %d is too large." % t.value
-		t.value = int(0)
-	return t
+    r'(0b)[01^(2-9)]+'
+    try:
+        t.value = int(t.value,2)
+    except ValueError:
+        print "Binary value %d is too large." % t.value
+        t.value = int(0)
+    return t
 
 # integers
 def t_INT(t):
-	r'[0-9][0-9]*'
-	try:
-		t.value = int(t.value)
-	except ValueError:
-		print "Integer value %d is too large." % t.value
-		t.value = int(0)
-	return t
+    r'[0-9][0-9]*'
+    try:
+        t.value = int(t.value)
+    except ValueError:
+        print "Integer value %d is too large." % t.value
+        t.value = int(0)
+    return t
 
 # booleans 
 def t_TRUE(t):
-	r'([tT][rR][uU][eE])' 
-	t.value = int(1)
-	return t 
+    r'([tT][rR][uU][eE])' 
+    t.value = int(1)
+    return t 
 
 def t_FALSE(t):
-	r'([fF][aA][lL][sS][eE])'
-	t.value = int(0)
-	return t
+    r'([fF][aA][lL][sS][eE])'
+    t.value = int(0)
+    return t
+
+# tokens to ignore
+t_ignore = " \t"
 
 # new lines
 def t_newline(t):
-	r'\n+'
-	t.lexer.lineno += t.value.count("\n")
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
 
 # tokens that have no type
 def t_error(t):
-	print "Illegal character '%s'!" % t.value[0]
-	t.lexer.skip(1)
+    print "Illegal character '%s'!" % t.value[0]
+    t.lexer.skip(1)
 
 # -----------------------------------------------------------------------------
 # Run PLY lex module that uses above functions to define tokens.
