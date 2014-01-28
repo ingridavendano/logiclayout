206a3fba427fe7e747095aef8b1b0307c41e2699
Ingrid Avendaño <ingridavendano@gmail.com>
2014-01-28 13:01:13 -0800

    Catches "expression operator" errors.

--- compiler/parser.py
+++ compiler/parser.py
@@ -27,20 +27,21 @@ root = [ ]
 
 def p_statement_assign(t):
     'statement : ID EQUALS expression'
-    print "issue5"
-    if type(t[1]) == str:
-        id_node = Id(t[1])
-        equals_node = Equals(id_node, t[3])
+    if t[3].expr == 'error':
+        equals_node = Break()
     else:
-        equals_node = Equals(t[1], t[3])
-    t[0] = equals_node
-    
+        if type(t[1]) == str:
+            id_node = Id(t[1])
+            equals_node = Equals(id_node, t[3])
+        else:
+            equals_node = Equals(t[1], t[3])
+        root.append(equals_node)
+
     ids[t[1]] = t[3]
-    root.append(equals_node)
+    t[0] = equals_node
 
 def p_statement_expr(t):
     'statement : expression'
-    print "issue3"
     root.append(t[1])
 
 def p_expression_not(t):
@@ -53,9 +54,9 @@ def p_expression_nand(t):
     t[0] = Nand(t[1], t[3])
 
 def p_expression_and(t):
-    '''expression : expression AND expression
-                  | ID AND expression'''
-    print "issue4"
+    # '''expression : expression AND expression
+    #               | ID AND expression'''
+    'expression : expression AND expression'
     t[0] = And(t[1], t[3])
 
 def p_expression_nor(t):
@@ -93,19 +94,22 @@ def p_expression_literal(t):
 
 def p_expression_id(t):
     'expression : ID'
-    print "issue1"
     try:
         variable = ids[t[1]]
     except LookupError:
         variable = Literal()
-        print "issue2"
     id_node = Id(t[1], variable)
     t[0] = id_node
 
 def p_expression_broken(t):
-    '''expression : expression AND'''
-    print "BREAK"
-    t[0] = ''
+    '''expression : expression AND
+                  | expression OR
+                  | expression XOR
+                  | expression NAND
+                  | expression NOR
+                  | expression NXOR'''
+    print "This expression is not valid."
+    t[0] = Break()
 
 def p_error(t):
     print "Syntax error at '%s'." % t.value
